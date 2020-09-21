e3ec6ae59d956b97478b23f5d36e86b4d220cb94
Your Name <you@example.com>
2020-09-21 15:20:03 +0300

    first public folder

---
+++ public/precache-manifest.37ff0ad09bd4a95f8a2c7b2bb66f9043.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73df3ed57c894a66d1c16cf3765179ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7795318f3503a69009e",
+    "url": "/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "d91ec2f09fc7c57118b0",
+    "url": "/static/css/main.ac1c87c7.chunk.css"
+  },
+  {
+    "revision": "b7795318f3503a69009e",
+    "url": "/static/js/2.75fca211.chunk.js"
+  },
+  {
+    "revision": "1b4685d8d7deebc6158958727421edd7",
+    "url": "/static/js/2.75fca211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91ec2f09fc7c57118b0",
+    "url": "/static/js/main.196c8e9c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/font1.ee653992.ttf"
+  }
+]);
