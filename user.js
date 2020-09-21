80e602663d46ba4f798075b7e863883f7a444a3c
Your Name <you@example.com>
2020-09-21 08:40:06 +0300

    m

---
+++ user.js
@@ -0,0 +1,21 @@
+let user ={
+    _id: 'asfasdfqw12412d1wd',
+    username: 'osnao',
+    fullName: 'my name',
+    password: 'saujsoaisnfoaie',
+    email: 'myEmail@gmail.com',
+    imgUrl: 'www.imgur.com/asfasf',
+    isAdmin: true,
+    boards: [{id: 212, name: 'board1' }],
+    notifications: [],
+    birthDay: '2nd August 1997',
+    company: 'adidas',
+    phoneNumber: 022412124
+}
+
+//minimal user
+let minUser = {
+    _id: 'asfasdfqw12412d1wd',
+    fullName: 'my name',
+    imgUrl: 'www.imgur.com/asfasf',
+}
