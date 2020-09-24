843c7d74894df39fb094dd304239284f2a463ea9
Your Name <you@example.com>
2020-09-24 13:41:33 +0300

    m

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2bb2dfe562a2e2a395b0a5b6a71e1127.js"
+  "/precache-manifest.723ac54155b816baffd71af9ba36a879.js"
 );
 
 self.addEventListener('message', (event) => {
