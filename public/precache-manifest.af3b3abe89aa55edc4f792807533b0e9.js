fe54cc047406072bced28518ac18e832900f6e27
Your Name <you@example.com>
2020-09-28 13:57:14 +0300

    m

---
+++ public/precache-manifest.af3b3abe89aa55edc4f792807533b0e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384d51aa6fc437a10e6bac8256ac7fab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e12f320b61226a4b6e1e",
+    "url": "/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "2645c5af95aac777cdcd",
+    "url": "/static/css/main.19b84be3.chunk.css"
+  },
+  {
+    "revision": "e12f320b61226a4b6e1e",
+    "url": "/static/js/2.934f3f76.chunk.js"
+  },
+  {
+    "revision": "e546b0085a9d51b817e9464528542a6c",
+    "url": "/static/js/2.934f3f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2645c5af95aac777cdcd",
+    "url": "/static/js/main.98312feb.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/font1.ee653992.ttf"
+  }
+]);
